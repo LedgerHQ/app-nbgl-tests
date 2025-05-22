c48b34d21a08bdb7f37224f0c64fbbb4f47f8b98
tdejoigny-ledger <thibault.de-joigny@ledger.fr>
2025-05-22 10:33:02 +0200

    apex p support (code & tests)

--- src/ui/menu.h
+++ src/ui/menu.h
@@ -9,7 +9,7 @@
 #define ICON_SOLANA   C_solana_14px
 #define ICON_POLYGON  C_polygon_14px
 #define ICON_ETHEREUM C_ethereum_14px
-#else
+#elif defined(TARGET_STAX) || defined(TARGET_FLEX)
 #if (APP_TYPE == APP_DEMO_TYPE)
 #define ICON_APP C_app_demo_64px
 #else
@@ -22,4 +22,17 @@
 #define ICON_SOLANA   C_solana_64px
 #define ICON_POLYGON  C_polygon_64px
 #define ICON_ETHEREUM C_ethereum_64px
+#elif defined(TARGET_APEX_P) 
+#if (APP_TYPE == APP_DEMO_TYPE)
+#define ICON_APP C_app_demo_48px
+#else
+#define ICON_APP C_app_nbgl_tests_48px
+#endif
+#define ICON_HOME     ICON_APP
+#define ICON_WARNING  LARGE_WARNING_ICON
+#define ICON_INFO     C_info_button_48px
+#define ICON_BITCOIN  C_bitcoin_48px
+#define ICON_SOLANA   C_solana_48px
+#define ICON_POLYGON  C_polygon_48px
+#define ICON_ETHEREUM C_ethereum_48px
 #endif
--- tests/test_demo_flows.py
+++ tests/test_demo_flows.py
@@ -17,6 +17,11 @@ def test_app_demo_flow_send_BTC(navigator: Navigator,
             NavInsID.BOTH_CLICK,
             NavInsID.BOTH_CLICK,
         ]
+    elif device.type == DeviceType.APEX_P:
+        instructions = [
+            NavInsID.USE_CASE_CHOICE_CONFIRM,
+            NavIns(NavInsID.TOUCH, (140, 90)),
+        ]
     else:
         instructions = [
             NavInsID.USE_CASE_CHOICE_CONFIRM,
@@ -41,8 +46,16 @@ def test_app_demo_flow_swap_1inch(navigator: Navigator,
             NavInsID.RIGHT_CLICK,
             NavInsID.BOTH_CLICK,
         ]
+    elif device.type == DeviceType.APEX_P:
+        instructions = [
+            NavInsID.USE_CASE_CHOICE_CONFIRM,
+            NavIns(NavInsID.TOUCH, (140, 155)),
+            NavInsID.USE_CASE_CHOICE_CONFIRM,
+            NavIns(NavInsID.TOUCH, (140, 210)),
+            NavIns(NavInsID.TOUCH, (30, 30)),
+            NavIns(NavInsID.TOUCH, (30, 30)),
+        ]
     else:
-        # Navigate in the main menu
         instructions = [
             NavInsID.USE_CASE_CHOICE_CONFIRM,
             NavIns(NavInsID.TOUCH, (200, 230)),  # Select 1inch
@@ -74,8 +87,12 @@ def test_app_demo_flow_BS_stake(navigator: Navigator,
             NavInsID.RIGHT_CLICK,
             NavInsID.BOTH_CLICK,
         ]
+    elif device.type == DeviceType.APEX_P:
+        instructions = [
+            NavInsID.USE_CASE_CHOICE_CONFIRM,
+            NavIns(NavInsID.TOUCH, (140, 225))
+        ]
     else:
-        # Navigate in the main menu
         instructions = [
             NavInsID.USE_CASE_CHOICE_CONFIRM,
             NavIns(NavInsID.TOUCH, (200, 320))
@@ -101,8 +118,12 @@ def test_app_demo_flow_SOL_receive(navigator: Navigator,
             NavInsID.RIGHT_CLICK,
             NavInsID.BOTH_CLICK,
         ]
+    elif device.type == DeviceType.APEX_P:
+        instructions = [
+            NavInsID.USE_CASE_CHOICE_CONFIRM,
+            NavIns(NavInsID.TOUCH, (140, 285))
+        ]
     else:
-        # Navigate in the main menu
         instructions = [
             NavInsID.USE_CASE_CHOICE_CONFIRM,
             NavIns(NavInsID.TOUCH, (200, 420))
@@ -120,33 +141,59 @@ def test_app_demo_flow_ETH_warning(device: Device,
                                    default_screenshot_path: str) -> None:
     if device.is_nano:
         pytest.skip("Nano does not support this use case with warning screen")
+    
 
-    instructions = [
+    if device.type == DeviceType.APEX_P:
+        instructions = [
             NavInsID.USE_CASE_CHOICE_CONFIRM,
-            NavIns(NavInsID.TOUCH, (200, 520)),
+            NavIns(NavInsID.TOUCH, (140, 350)),
             NavInsID.INFO_HEADER_TAP,
             NavInsID.LEFT_HEADER_TAP,
             NavInsID.USE_CASE_REVIEW_REJECT,
             NavInsID.INFO_HEADER_TAP,
-            NavIns(NavInsID.TOUCH, (200, 150)),
+            NavIns(NavInsID.TOUCH, (140, 90)),
             NavInsID.LEFT_HEADER_TAP,
             NavInsID.LEFT_HEADER_TAP,
             NavInsID.USE_CASE_CHOICE_CONFIRM,
-            NavIns(NavInsID.TOUCH, (200, 150)),
+            NavIns(NavInsID.TOUCH, (140, 90)),
             NavInsID.LEFT_HEADER_TAP,
             NavInsID.LEFT_HEADER_TAP,
             NavInsID.SWIPE_CENTER_TO_LEFT,
             NavInsID.SWIPE_CENTER_TO_LEFT,
-    ]
-    if device.type == DeviceType.FLEX:
-        instructions += [NavInsID.SWIPE_CENTER_TO_LEFT]
-    instructions += [
+            NavInsID.SWIPE_CENTER_TO_LEFT,
             NavInsID.INFO_HEADER_TAP,
-            NavIns(NavInsID.TOUCH, (200, 150)),
+            NavIns(NavInsID.TOUCH, (140, 90)),
             NavInsID.LEFT_HEADER_TAP,
             NavInsID.LEFT_HEADER_TAP,
             NavInsID.USE_CASE_REVIEW_CONFIRM,
         ]
+    else:
+        instructions = [
+                NavInsID.USE_CASE_CHOICE_CONFIRM,
+                NavIns(NavInsID.TOUCH, (200, 520)),
+                NavInsID.INFO_HEADER_TAP,
+                NavInsID.LEFT_HEADER_TAP,
+                NavInsID.USE_CASE_REVIEW_REJECT,
+                NavInsID.INFO_HEADER_TAP,
+                NavIns(NavInsID.TOUCH, (200, 150)),
+                NavInsID.LEFT_HEADER_TAP,
+                NavInsID.LEFT_HEADER_TAP,
+                NavInsID.USE_CASE_CHOICE_CONFIRM,
+                NavIns(NavInsID.TOUCH, (200, 150)),
+                NavInsID.LEFT_HEADER_TAP,
+                NavInsID.LEFT_HEADER_TAP,
+                NavInsID.SWIPE_CENTER_TO_LEFT,
+                NavInsID.SWIPE_CENTER_TO_LEFT,
+        ]
+        if device.type == DeviceType.FLEX:
+            instructions += [NavInsID.SWIPE_CENTER_TO_LEFT]
+        instructions += [
+                NavInsID.INFO_HEADER_TAP,
+                NavIns(NavInsID.TOUCH, (200, 150)),
+                NavInsID.LEFT_HEADER_TAP,
+                NavInsID.LEFT_HEADER_TAP,
+                NavInsID.USE_CASE_REVIEW_CONFIRM,
+            ]
     navigator.navigate_and_compare(default_screenshot_path, test_name, instructions,
                                    screen_change_before_first_instruction=False,
                                    screen_change_after_last_instruction=False)
